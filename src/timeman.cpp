500af261887caa545fe86cebec8cc35b3204b7dc
e <e@e.e>
2022-02-16 13:29:53 -0500

    local tune 3

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -73,7 +73,7 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, Position& pos, i
 
   // Scale time with current complexity
   int complexity = pos.this_thread()->complexityAverage.value();
-  optExtra *= std::clamp(1.0 + (complexity - 753) / 3660, 0.94, 1.17);
+  optExtra *= std::clamp(1.0 + (complexity - 789) / 457, 0.87, 1.13);
 
   // A user may scale time usage by setting UCI option "Slow Mover"
   // Default is 100 and changing this value will probably lose elo.
